f0d1c3be41b4f02c903e7186db8091bfc0e0ab81
Melquisedeque <melquisedequebass@gmail.com>
2026-09-08 19:15:15 -0300

    adicionar mensagem de dias em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-  return <h1>Test - energy tracker</h1>;
+  function getTime() {
+    const startDate = new Date("2026-08-15").getTime();
+    const now = new Date().getTime();
+    const diff = now - startDate;
+
+    let daysDiff = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    return daysDiff;
+  }
+
+  return <h1>Em construção há {getTime()} dias</h1>;
 }
 
 export default Home;
